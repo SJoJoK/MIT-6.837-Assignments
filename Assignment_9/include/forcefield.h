cb72086f20b8fb272b396c226d456ffe1273f5f2
SJoJoK <450986321@qq.com>
2021-09-05 22:10:22 +0800

    forcefield完成

--- Assignment_9/include/forcefield.h
+++ Assignment_9/include/forcefield.h
@@ -1,3 +1,4 @@
+#include "vectors.h"
 class ForceField
 {
 public:
@@ -5,14 +6,54 @@ public:
 };
 class GravityForceField : public ForceField
 {
+public:
+    Vec3f g;
+    GravityForceField(Vec3f gravity)
+    {
+        this->g = gravity;
+    };
+    virtual Vec3f getAcceleration(const Vec3f &position, float mass, float t)
+    {
+        return g;
+    }
 };
 
 class ConstantForceField : public ForceField
 {
+public:
+    Vec3f f;
+    ConstantForceField(Vec3f force)
+    {
+        this->f = force;
+    };
+    virtual Vec3f getAcceleration(const Vec3f &position, float mass, float t)
+    {
+        return f * (1/mass);
+    }
 };
 class RadialForceField : public ForceField
 {
+public:
+    float mag;
+    RadialForceField(float magnitude)
+    {
+        this->mag = magnitude;
+    };
+    virtual Vec3f getAcceleration(const Vec3f &position, float mass, float t)
+    {
+        return mag * (Vec3f(0, 0, 0) - position);
+    }
 };
 class VerticalForceField : public ForceField
 {
+public:
+    float mag;
+    VerticalForceField(float magnitude)
+    {
+        this->mag = magnitude;
+    };
+    virtual Vec3f getAcceleration(const Vec3f &position, float mass, float t)
+    {
+        return mag * (Vec3f(position.x(), 0, position.z()));
+    }
 };
